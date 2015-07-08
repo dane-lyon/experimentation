38f90f11cc54dc15d6fc0d570d2b83c5301ecf76
DANE <dane-lyon@users.noreply.github.com>
2015-07-08 13:52:58 +0200

    Update test_backup_sacoche.sh

--- test_backup_sacoche.sh
+++ test_backup_sacoche.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-# Version 2
 # Dane de Lyon
 # Ce script est a placer dans le dossier /root/drt, ne pas oublier les droits d'execution (+x) !
 # exemple de cron pour lancer tous les jours a 6H du matin (/etc/cron.d/backup_opener) : 
-# 0 6 * * * root /root/drt/backup_openerp.sh
+# 0 4 * * * root /root/drt/backup_sacoche.sh
 
 . ParseDico
 
-cd /home/openerp_bdd/base
+#mkdir /home/backup && mkdir /home/backup/base_sacoche
+cd /home/backup/base_sacoche
 
 # Lancement avec le compte postgres
-if su postgres -c "pg_dumpall > backup_openerp.out 2>backup.erreur" ; then
+su postgres -c "pg_dumpall > backup_sacoche.out 
 
 # Compression du fichier de sauvegarde avec la date du jour
-tar czvf backup_openerp-`date +%Y-%m-%d`.tar.gz backup_openerp.out
-rm -f backup_openerp.out
+tar czvf backup_sacoche-`date +%Y-%m-%d`.tar.gz backup_sacoche.out
+rm -f backup_sacoche.out
 
 # Placement du backup au bon endroit
-mv -f *.tar.gz /home/openerp_bdd
+mv -f *.tar.gz /home/backup/base_sacoche/
 
-# Purge des anciens backups OpenERP qui ont plus de 20 jours
-find /home/openerp_bdd/backup_openerp-* -type f -mtime +20 -exec rm -rf {} \;
-
-else
-
-echo "Il y a eu une erreur lors du backup postgresql d'OpenERP pour l'établissement $libelle_etab ($numero_etab), vous pouvez consulter le fichier backup.erreur dans /home/openerp_base/base pour plus de précision" | mutt -s "MAIL" dane@ac-lyon.fr
-
-fi
+# Purge des anciens backups OpenERP qui ont plus de 30 jours
+find /home/backup/base_sacoche/backup_sacoche-* -type f -mtime +30 -exec rm -rf {} \;
 
 exit
