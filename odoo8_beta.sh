035716894de5fd5b8bfd64841f43122c6198f186
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-09-09 12:36:13 +0200

    Update odoo8_beta.sh

--- odoo8_beta.sh
+++ odoo8_beta.sh
@@ -2,20 +2,22 @@
 
 #### script pour installer Odoo 8 sur scribe 2.4 #######
 #### DANE rectorat de lyon ######
-#### Dominique J, Simon B, Jean-Philippe.P ####
-#### Version 4 ######
+#### Simon B, Dominique J, Jean-Philippe.P ####
+
+
+#### Version 4.0 spécifique a Odoo 8 pour Scribe 2.4 ######
+# Script modifié pour la nouvelle version par Simon.B avec l'aide de Karim.A
 
 #### on teste si le paquet est présent inutile d'aller plus loin si c'est la cas
-#dpkg -s odoo &>/dev/null
-#if [ $? -eq 0 ] ; then
-#	echo "odoo est déjà présent"
-#	exit 0
-#else
+dpkg -s odoo &>/dev/null
+if [ $? -eq 0 ] ; then
+	echo "odoo est déjà présent"
+	exit 0
+else
 
 
-#### on rajoute les outils eole pour lancer postgresql et Odoo
-# désactivé pour scribe 2.4 car message indique que ça ne doit plus être utilisé
-#. /usr/share/eole/FonctionsEoleNg
+#### on rajoute les outils eole 
+. /usr/lib/eole/ihm.sh
 
 
 ##### on installe Odoo
@@ -25,19 +27,14 @@ dpkg -i odoo_8.0.20150908_all.deb
 apt-get -fy install
 
 #### ouverture des ports sur le scribe, penser au reconfigure a la fin
-echo "allow_src(interface='eth0', ip='0/0', port='8069')
-allow_src(interface='eth0', ip='0/0', port='5432')" > /usr/share/eole/firewall/00_root_odoo.fw
-#donc on ajoute les autorisations à la volée
-/sbin/iptables -I wide-root -p tcp -m state --state NEW -m tcp --dport 8069 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT
-/sbin/iptables -I wide-root -p tcp -m state --state NEW -m tcp --dport 5432 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT
-
+# La méthode a changé en Scribe 2.4, il faut utiliser maintenant le fichier dico.xml déployé par Zephir
 
 #### paramétrage de postgres pour le rendre accessible depuis le réseau
 ### on remplace #listen_addresses = 'localhost' par listen_addresses = '*' dans ####le fichier de conf pour que le serveur écoute
 
 sed -i.BAK  "s/^\#listen_addresses =.*/listen_addresses = '*'/g" /etc/postgresql/9.1/main/postgresql.conf
-### modification du fichier pg_hba.conf
 
+### modification du fichier pg_hba.conf
 #### attention je passe par un numéro de ligne donc cette opération peut échouer
 sed -i.BAK "85i\host    all        all    0.0.0.0/0    trust" /etc/postgresql/9.1/main/pg_hba.conf
 
@@ -54,18 +51,18 @@ File = /var/lib/postgresql
 }" /etc/bacula/baculafichiers.d/odoo.conf
 
 #### on rajoute les tests dans le diagnose
-# bon de toute façon cette partie ça peux pas marcher pour odoo pour le diagnose pour l'instant...
 echo "#! /bin/bash
-. /usr/share/eole/FonctionsEoleNg
+. /usr/lib/eole/ihm.sh
 . /etc/eole/containers.conf
-. ParseDico
+# $(CreoleGet nom_variable)
+
 if [ \$activer_mysql == 'oui' ]; then
 EchoGras \"*** Odoo\"
 TestPid PostgreSql postgres
 fi
 
 if [ \"\$activer_apache\" != \"non\" ];then
-	. /usr/share/eole/FonctionsEoleNg
+	. /usr/lib/eole/ihm.sh
 	if [ \$adresse_ip_web = '127.0.0.1' ];then
 	TestHTTPPage Odoo http://\$adresse_ip_eth0:8069
 	else
@@ -83,8 +80,8 @@ echo "un log contenant les dossiers modifiés est disponible sur le perso de l'a
 
 echo "les fichiers modifiés par cette installation sont:
 /usr/share/eole/firewall/00_root_odoo.fw
-/etc/postgresql/8.4/main/postgresql.conf
-/etc/postgresql/8.4/main/pg_hba.conf
+/etc/postgresql/9.1/main/postgresql.conf
+/etc/postgresql/9.1/main/pg_hba.conf
 /etc/bacula/baculafichiers.d/odoo.conf
 /usr/share/eole/diagnose/module/151-odoo
 
