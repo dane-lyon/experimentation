bebc691cfeb06a6bd0f5e199dc7ac005dec04d56
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-10-08 10:37:17 +0200

    Update test-deb-post-i.sh

--- test-deb-post-i.sh
+++ test-deb-post-i.sh
@@ -1,3 +1,24 @@
 # NE PAS UTILISER CE SCRIPT, IL N'EST PAS TERMINE, IL NE FONCTIONNE DONC PAS TANT QUE VOUS VOYEZ CE MESSAGE !!!!
 
+# Dépot & Mise a jour
+wget {{lien a mettre ici sources.list}}
+mv -f sources.list /etc/apt/
+apt-get update 
+apt-get -y install pkg-mozilla-archive-keyring deb-multimedia-keyring
+apt-get update
+apt-get -y dist-upgrade
+
+# Activation du multiarch 32b 
+dpkg --add-architecture i386
+apt-get update
+
+# Backportage de Iceweasel alias Firefox 
+apt-get -t jessie-backports install iceweasel iceweasel-l10n-fr
+
+# Backportage de LibreOffice
+apt-get -t jessie-backports install libreoffice libreoffice-gtk libreoffice-l10n-fr libreoffice-help-fr
+
+[[ APPLI ]]
+
+
 
