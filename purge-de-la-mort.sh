d0a4a29bfada74a3f94167a9e1b0e17b17ed65a7
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-10-16 16:17:18 +0200

    Update purge-de-la-mort.sh

--- purge-de-la-mort.sh
+++ purge-de-la-mort.sh
@@ -9,8 +9,10 @@
 date >> /home/a/admin/perso/stat_purge.txt
 df -h /home >> /home/a/admin/perso/stat_purge.txt
 
-#purge de la mort
+#purge de la mort 
 rm -rf /home/*/*/perso/{Application\ Data,config_eole,.Config}/*
+find /home -type f -name ".~lock.*" -exec rm -rf {} \;
+#find /home/workgroups/commun/logiciels/$google -iregex '^.*\.dat.*$' -exec rm -rf {} \;
 
 # info après la purge (date+heure+taille occupé du home)
 date >> /home/a/admin/perso/stat_purge.txt
