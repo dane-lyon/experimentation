74c9a97b927613088ad7223530cd8f388e63bc96
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-10-16 16:01:42 +0200

    Create purge-de-la-mort.sh

---
+++ purge-de-la-mort.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Voici une purge radicale, elle supprime complètement :
+
+# - tous le contenu présent dans les dossiers ".Config" et/ou "Application Data" et/ou "config_eole" de tous les utilisateurs.
+# - une statistique stat_purge.txt dans le perso de l'admin qui affiche la taille du home avant & après la purge
+
+# info avant la purge (date+heure+taille occupé du home)
+date >> /home/a/admin/perso/stat_purge.txt
+df -h /home >> /home/a/admin/perso/stat_purge.txt
+
+#purge de la mort
+rm -rf /home/*/*/perso/{Application\ Data,config_eole,.Config}/*
+
+# info après la purge (date+heure+taille occupé du home)
+date >> /home/a/admin/perso/stat_purge.txt
+df -h /home >> /home/a/admin/perso/stat_purge.txt
