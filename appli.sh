0fadef56f6eba2a0ac46b98b1cafff2d4ef9107d
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-11-18 15:15:37 +0100

    Create appli.sh

---
+++ appli.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Script crée par Simon BERNARD
+# précision : non terminé
+
+# -----------------------------------------------------
+# choix du profil
+# -----------------------------------------------------
+
+echo "Voici la liste des profils d'applications diposnibles : "
+
+echo "a = établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante"
+echo "b = établissement scolaire - distribution Debian Jessie (8) ou variante"
+echo "c = technicien sous linux / DANE - distribution Ubuntu Trusty (14.04) ou variante"
+echo "d = technicien sous linux / DANE - distribution Debian Jessie (8) ou variante"
+echo "e = utilisation personnelle - distribution Ubuntu Trusty (14.04) ou variante"
+echo "f = utilisation personnelle - distribution Debian Jessie (8) ou variante"
+echo "g = utilisation sous Archlinux"
+echo "h = utilisation familiale - distribution Ubuntu Trusty (14.04) ou variante"
+echo "i = profil libre non défini 1"
+echo "j = profil libre non défini 2"
+echo "k = profil libre non défini 3"
+
+read -p "Répondre par la lettre correspondante en minuscule (exemple : f) : " rep
+
+
+# -----------------------------------------------------
+# Profil "a" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "a" ] ; then
+        echo "profil a"
+else
+
+# -----------------------------------------------------
+# Profil "b" : établissement scolaire - distribution Debian Jessie (8) ou variante"
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else     
+
+# -----------------------------------------------------
+# Profil "c" : technicien sous linux / DANE - distribution Ubuntu Trusty (14.04) ou variante"
+# -----------------------------------------------------
+
+if [ "$rep" = "c" ] ; then
+                echo "profil c"
+else  
+
+# -----------------------------------------------------
+# Profil "d" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else  
+
+# -----------------------------------------------------
+# Profil "e" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else  
+
+# -----------------------------------------------------
+# Profil "f" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else  
+
+# -----------------------------------------------------
+# Profil "g" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else  
+
+# -----------------------------------------------------
+# Profil "h" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else  
+
+# -----------------------------------------------------
+# Profil "i" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else  
+                
+# -----------------------------------------------------
+# Profil "j" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else                  
+                
+# -----------------------------------------------------
+# Profil "k" : établissement scolaire - distribution Ubuntu Trusty (14.04) ou variante
+# -----------------------------------------------------
+
+if [ "$rep" = "b" ] ; then
+                echo "profil b"
+else  
