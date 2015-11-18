5b92944b4c1bd6bd2bdeafee20025363d61989c4
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-11-18 14:28:42 +0100

    Update test_condition_extinction.sh

--- test_condition_extinction.sh
+++ test_condition_extinction.sh
@@ -2,24 +2,20 @@
 
 # cron d'extinction automatique a lancer ?
 
-echo "Pour terminer, voulez vous activer l'extinction automatique des postes le soir dans le cas ou des pc auraient oubliés d'être éteints par les élèves ?"
+echo "Pour terminer, voulez vous activer l'extinction automatique des postes le soir ?"
 echo "0 ou aucune valeure saisie = non, pas d'extinction auto le soir"
 echo "1 = oui, extinction a 19H00"
 echo "2 = oui, extinction a 20H00"
-echo "3 = oui, extinction a 21H00"
-echo "4 = oui, extinction a 23H00"
-read -p "Répondre par le chiffre correspondant (0,1,2,3,4) : " rep_proghalt
+echo "3 = oui, extinction a 22H00"
+read -p "Répondre par le chiffre correspondant (0,1,2,3) : " rep_proghalt
 
 if [ "$rep_proghalt" = "1" ] ; then
-        echo "val1" 
+        echo "0 19 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
         else if [ "$rep_proghalt" = "2" ] ; then
-                echo "val2"
+                echo "0 20 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
                 else if [ "$rep_proghalt" = "3" ] ; then
-                        echo "val3"
-                        else if [ "$rep_proghalt" = "4" ] ; then
-                                echo "val4"
-                             fi
-                      fi
+                        echo "0 22 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
+                     fi
              fi
 fi
 
