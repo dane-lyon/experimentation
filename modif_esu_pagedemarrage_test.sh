61d9cba4dfddf65376c92f69dd855e52aa6e29a0
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-01-28 14:12:50 +0100

    Update modif_esu_pagedemarrage_test.sh

--- modif_esu_pagedemarrage_test.sh
+++ modif_esu_pagedemarrage_test.sh
@@ -4,5 +4,5 @@ cp -a /home/esu /home/esu.backup
 read -p "Veuillez indiquer l'ancien url par défaut sans le http:// devant (exemple : google.fr)" oldurl
 read -p "Donnez la valeur du nouveau url sans le http:// devant (exemple : lite.qwant.com) :" newurl
 # remplacement de l'ancienne valeur par la nouvelle valeur dans la configuration ESU existante
-sed -i  "s/$oldurl/$newurl/g" /home/esu/Base/*/*xml
+sed -i  "s|$oldurl|$newurl|g" /home/esu/Base/*/*xml
 exit 0
