cfbebb3c0eaec9aff160e7e21f1d57bf941b9e51
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-02-05 16:36:32 +0100

    Update modif_page_accueil.sh

--- modif_page_accueil.sh
+++ modif_page_accueil.sh
@@ -2,6 +2,8 @@
 
 ########ce script permet de modifier dans tous les groupes machines, de remplacer la page d'accueil google.fr par la page lite.qwant.com
 ########  © Virginie Favrat & Jean philippe Patrizio DANE
- 
+
+# on sauvegarde le dossier ESU
+cp - r /home/esu /home/esu.bak
 sed -i 's/\(<Variable nom="browser.startup.homepage.*>\)\(.*\)\(<\/Variable>\)/\1 https:\/\/lite.qwant.com \3/' /home/esu/Base/*/*xml
-sed -i 's/\(<Variable nom="Start Page.*>\)\(.*\)\(<\/Variable>\)/\1 https:\/\/lite.qwant.com \3/' /home/esu/Base/*/*xml
+sed -i 's/\(<Variable nom="Start Page.*>\)\(.*\)\(<\/Variable>\)/\1 lite.qwant.com \3/' /home/esu/Base/*/*xml
