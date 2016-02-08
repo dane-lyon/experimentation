df65c0ba0fe9ea1a0339044edf6fa0cd100e1c9a
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-02-08 14:08:36 +0100

    Delete modif_esu_pagedemarrage_test.sh

--- modif_esu_pagedemarrage_test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# remplacement valeur de la page url dans  la configuration ESU
-cp -a /home/esu /home/esu.backup
-read -p "Veuillez indiquer l'ancien url par défaut sans le http:// devant (exemple : google.fr)" oldurl
-read -p "Donnez la valeur du nouveau url sans le http:// devant (exemple : lite.qwant.com) :" newurl
-# remplacement de l'ancienne valeur par la nouvelle valeur dans la configuration ESU existante
-sed -i  "s|$oldurl|$newurl|g" /home/esu/Base/*/*xml
-exit 0
