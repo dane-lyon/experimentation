6b038cebd089ed108bf75133f8d4ead26f8134d2
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-02-09 10:02:50 +0100

    suppression espaces

--- modif_page_accueil.sh
+++ modif_page_accueil.sh
@@ -1,9 +1,9 @@
 # /bin/bash
 
-########ce script permet de modifier dans tous les groupes machines, de remplacer la page d'accueil google.fr par la page lite.qwant.com
-########  © Virginie Favrat & Jean philippe Patrizio DANE
+######## ce script permet de modifier dans tous les groupes machines, de remplacer la page d'accueil google.fr par la page lite.qwant.com
+######## Virginie Favrat & Jean philippe Patrizio - DANE de Lyon
 
 # on sauvegarde le dossier ESU
 cp -r /home/esu /home/esu.bak
-sed -i 's/\(<Variable nom="browser.startup.homepage.*>\)\(.*\)\(<\/Variable>\)/\1 https:\/\/lite.qwant.com \3/' /home/esu/Base/*/*xml
-sed -i 's/\(<Variable nom="Start Page.*>\)\(.*\)\(<\/Variable>\)/\1https:\/\/lite.qwant.com \3/' /home/esu/Base/*/*xml
+sed -i 's/\(<Variable nom="browser.startup.homepage.*>\)\(.*\)\(<\/Variable>\)/\1https:\/\/lite.qwant.com\3/' /home/esu/Base/*/*xml
+sed -i 's/\(<Variable nom="Start Page.*>\)\(.*\)\(<\/Variable>\)/\1https:\/\/lite.qwant.com\3/' /home/esu/Base/*/*xml
