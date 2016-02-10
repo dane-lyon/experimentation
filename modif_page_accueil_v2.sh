a082b844a99b5bb570642dbc3e7fc2276432593a
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-02-10 09:29:14 +0100

    Create modif_page_accueil_v2.sh

---
+++ modif_page_accueil_v2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+########### ce script est pour modifier la page d'accueil de firefox et I.E. #############
+###########  DANE rectorat de lyon #########
+###########  Virginie  Favrat  &  Jean Philippe Patrizio. ########
+
+
+########### on efface l'ecran
+clear
+
+echo -e "\n\n\n\n"
+
+echo "ce script permet de modifier dans tous les groupes machines, la page d'accueil google.fr par la pages de votre choix."
+
+#### on sauvegarde le dossier esu
+
+cp -r /home/esu /home/esu.bak
+
+echo -e "\n\n"
+
+read -p "		Saisissez le domaine ex: (https://lite.qwant.com) que vous souhaitez déployer : " domaine
+
+#### Attention on échape les caractères // avec # comme séparateur.
+
+domaine=$(echo $domaine | sed 's#/#\\/#g')
+
+#### On remplace
+
+sed -i 's/\(<Variable nom="Start Page.*>\)\(.*\)\(<\/Variable>\)/\1'$domaine'\3/' /home/esu/Base/*/*xml
+
+sed -i 's/\(<Variable nom="browser.startup.homepage.*>\)\(.*\)\(<\/Variable>\)/\1'$domaine'\3/' /home/esu/Base/*/*xml
