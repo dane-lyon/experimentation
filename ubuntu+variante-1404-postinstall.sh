cf7aeb26f440dd7ac1eda6d411e4ad5964260277
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-03-22 13:29:31 +0100

    Update ubuntu+variante-1404-postinstall.sh

--- ubuntu+variante-1404-postinstall.sh
+++ ubuntu+variante-1404-postinstall.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 
+# Concerne toutes les variantes
+
+
+
+
+
+
+
+
+# Concerne uniquement Ubuntu / Unity
+
+#si variante = ubu alors :
+
+
+
+
+
+
+
+
+# Concerne uniquement Xubuntu / XFCE
+
+#si variante = xub alors :
+
+
+# Customisation XFCE
+
+add-apt-repository -y ppa:docky-core/stable ; apt-get update ; apt-get -y install plank
+wget http://lien/skel.tar.gz ; tar xzvf skel.tar.gz -C /etc ; rm -rf skel.tar.gz
