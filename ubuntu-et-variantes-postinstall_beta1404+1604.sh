2563287c159747df51bfbdea8eccb695e70c8052
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-03-30 11:45:17 +0200

    Update ubuntu-et-variantes-postinstall_beta1404+1604.sh

--- ubuntu-et-variantes-postinstall_beta1404+1604.sh
+++ ubuntu-et-variantes-postinstall_beta1404+1604.sh
@@ -26,7 +26,7 @@ echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select tr
 
 # Oracle Java 8
 add-apt-repository -y ppa:webupd8team/java ; apt-get update ; echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections | apt-get -y install oracle-java8-installer
-
+# vérifier si ça marche sinon mettre fichier.list sur le github...
 
 #########################################
 # Paquet uniquement pour la 14.04 / 17.3
@@ -43,9 +43,9 @@ apt-get -y install idle-python3.4 gstreamer0.10-plugins-ugly celestia
 # Comme le PPA bloque avec un proxy actuellement, méthode alternative : [désactivé pour la 16.04]
 wget --no-check-certificate https://raw.githubusercontent.com/dane-lyon/fichier-de-config/master/libreoffice-ppa.list ;
 mv libreoffice-ppa.list /etc/apt/sources.list.d/ ;
-apt-get update ; apt-get -y --force-yes upgrade ; apt-get -y --force-yes install libreoffice libreoffice-gtk libreoffice-l10n-fr ;
+apt-get update ; apt-get -y --force-yes upgrade ; 
 
-# Pour Google Earth :
+# Pour Google Earth : #(cette méthode ne fonctionne pas sur la 16.04)
 apt-get -y install libfontconfig1:i386 libx11-6:i386 libxrender1:i386 libxext6:i386 libgl1-mesa-glx:i386 libglu1-mesa:i386 libglib2.0-0:i386 libsm6:i386
 wget https://dl.google.com/dl/earth/client/current/google-earth-stable_current_i386.deb ; dpkg -i google-earth-stable_current_i386.deb ; apt-get -fy install ; rm -f google-earth-stable_current_i386.deb 
 
@@ -58,14 +58,16 @@ if [ "$DISTRIB_RELEASE" = "16.04" ] || [ "$DISTRIB_RELEASE" = "18" ] ; then
 
 # paquet
 apt-get -y install idle-python3.5 x265 ;
-# google earth plus tard ici 
+# méthode d'installation alternative pour Google Earth ? pour Celestia ? 
 
 fi
 
 #=======================================================================================================#
+
 # Installation quelque soit la variante et la version 
 
 #[ Bureautique ]
+apt-get -y --force-yes install libreoffice libreoffice-gtk libreoffice-l10n-fr 
 apt-get -y install freeplane scribus gnote xournal cups-pdf
 
 #[ Web ]
@@ -106,7 +108,7 @@ if [ "$(which unity)" = "/usr/bin/unity" ] ; then  # si Ubuntu/Unity alors :
 apt-get -y install ubuntu-restricted-extras ubuntu-restricted-addons unity-tweak-tool
 apt-get -y install nautilus-image-converter nautilus-script-audio-convert
 
-# Icone Numis supplémentaire
+# Icone Numis supplémentaire # <= ajouter ds fichier.list ?
 #add-apt-repository -y ppa:numix/ppa ; apt-get --force-yes install -y numix-icon-theme-circle
 
 fi
@@ -122,7 +124,7 @@ apt-get -y install xubuntu-restricted-extras xubuntu-restricted-addons xfce4-goo
 # Customisation XFCE
 
 #add-apt-repository -y ppa:docky-core/stable ; apt-get update ; apt-get -y install plank --force-yes
-# PPA temporairement désactivé, méthode alternative :
+# PPA temporairement désactivé, méthode alternative : #uniquement pour 14.04
 wget --no-check-certificate https://raw.githubusercontent.com/dane-lyon/fichier-de-config/master/plank-ppa.list ;
 mv plank-ppa.list /etc/apt/sources.list.d/ ;
 apt-get update ; apt-get -y install plank --force-yes
@@ -139,13 +141,17 @@ apt-get -y install xubuntu-restricted-extras xubuntu-restricted-addons xfce4-goo
 wget --no-check-certificate https://dane.ac-lyon.fr/spip/IMG/tar/skel_xub1404.tar ; tar xvf skel_xub1404.tar -C /etc ; rm -rf skel_xub1404.tar
 fi
 
+################################
+# Concerne Ubuntu Mate / Mate
+################################
 if [ "$(which caja)" = "/usr/bin/caja" ] && [ "$DISTRIB_RELEASE" = "16.04" ] ; then # si Ubuntu Mate 16.04 alors :
 
 #paquet
 apt-get -y install ubuntu-restricted-extras mate-desktop-enivonment-extras
 apt-get -y purge cheese transmission-gtk pidgin hexchat rhythmbox
 
-# Skel spécifique a Mate ?
+# Customisation Mate
+# Mettre 1 skel spécifique a Mate ?
 
 fi
 
