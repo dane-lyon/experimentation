6f16140f39e856dec13da5dcdc50b5f9123e1304
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-06-17 09:38:21 +0200

    Add files via upload

---
+++ Installation-dane-util.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+
+#read -p "Veuillez indiquer l'ip du proxy :" ip_proxy
+#read -p "Veuillez indiquer le port du proxy :" port_proxy
+
+#export https_proxy=$ip_proxy:$port_proxy
+wget https://github.com/dane-lyon/experimentation/blob/master/dane-util_1.0.51.deb
+wget https://github.com/dane-lyon/experimentation/blob/master/dane-util.desktop
+dpkg -i dane-util.deb
+mv dane-util.desktop /usr/share/applications/
+
+
