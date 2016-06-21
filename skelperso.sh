38c371496fb21e80e684f9ba91483119fb7d862c
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-06-21 10:56:46 +0200

    Update skelperso.sh

--- skelperso.sh
+++ skelperso.sh
@@ -25,7 +25,7 @@ cp -rf ~/Bureau /etc/skel/ ;
 cp -rf ~/Desktop /etc/skel/ ;
 
 #############################################
-# Page par défaut Firefox (a décommenter si besoin)
+# Page par défaut Firefox #a décommenter si besoin
 #############################################
 #echo "user_pref(\"browser.startup.homepage\", \"http://lite.qwant.com\");" >> /usr/lib/firefox/defaults/pref/channel-prefs.js
 
