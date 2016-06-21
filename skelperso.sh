c9ba103f4bae2df1c8f4c3ff1367ad9a4b255879
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-06-21 10:29:31 +0200

    Update and rename trusty_custom.sh to skelperso.sh

---
+++ skelperso.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ce script est là pour automatiser la copie du profil en cours dans le répertoire skel afin d'avoir un bureau customisé
+#homogène sur l'ensemble du parc. Vous devez donc avoir personnalisé le bureau comme vous le souhaitez avant d'appliquer
+#ce script. 
+
+# Doit impérativement être lancé avec sudo !
+
+#############################################
+# Run using sudo, of course.
+#############################################
+if [ "$UID" -ne "0" ]
+then
+  echo "Il faut lancer ce script avec sudo => sudo ./skelperso.sh"
+  exit 
+fi
+
+#############################################
+# Copie votre profil
+#############################################
+cp -rf ~/.config /etc/skel/ ;
+cp -rf ~/.local /etc/skel/ ;
+cp -rf ~/.gconf /etc/skel/ ;
+cp -rf ~/Bureau /etc/skel/ ;
+cp -rf ~/Desktop /etc/skel/ ;
--- trusty_custom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ce script est là pour automatiser la copie du profil en cours dans le répertoire skel afin d'avoir un bureau customisé
-#homogène sur l'ensemble du parc. Vous devez donc avoir personnalisé le bureau comme vous le souhaitez avant d'appliquer
-#ce script.
-
-{ a faire ici }
