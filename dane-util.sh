c70893762b04765550604f00584eafd9c5bef04b
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-07-21 09:56:21 +0200

    Add files via upload

---
+++ dane-util.sh
@@ -0,0 +1,6 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo wget https://github.com/dane-lyon/experimentation/blob/master/dane-util_1.0.52-0ubuntu4_all.deb
+sudo dpkg -i dane-util_1.0.52-0ubuntu4_all.deb
+clear
+echo "vous trouvez  Utilitaire Dane dans le menu éducation."
