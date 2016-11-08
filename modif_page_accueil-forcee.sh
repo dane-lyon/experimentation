879fc0d30376b194640a1a51f244cc98673391c2
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-11-08 15:22:07 +0100

    Update modif_page_accueil-forcee.sh
    Ce script Force votre page d'accueil par lite.qwant.com

--- modif_page_accueil-forcee.sh
+++ modif_page_accueil-forcee.sh
@@ -1,4 +1,4 @@
-# /bin/bash
+# !/bin/bash
 
 ######## ce script permet de modifier dans tous les groupes machines, de remplacer la page d'accueil google.fr par la page lite.qwant.com
 ######## Virginie Favrat & Jean philippe Patrizio - DANE de Lyon
