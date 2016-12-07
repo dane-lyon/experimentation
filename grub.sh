f1a29268e463acf21a266b7a7321af2847410ada
Simon.B <sibe39@users.noreply.github.com>
2016-12-07 14:31:20 +0100

    Update grub.sh

--- grub.sh
+++ grub.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo 
-'"GRUB_DEFAULT=0
+wget --no-check-certificate https://raw.githubusercontent.com/dane-lyon/fichier-de-config/master/grub_custom.jpg
+mv -f grub_custom.jpg /usr/share/themes/
+
+echo 'GRUB_DEFAULT=0
 GRUB_TIMEOUT=0
 GRUB_HIDDEN_TIMEOUT=3
 GRUB_HIDDEN_TIMOUT_QUIET=false
 GRUB_DISABLE_RECOVERY=true
 GRUB_GFXMODE=640x480
-GRUB_BACKGROUND="/usr/share/oscar/icones/grub_custom.jpg"'
+GRUB_BACKGROUND="/usr/share/themes/grub_custom.jpg"' > /etc/default/grub
