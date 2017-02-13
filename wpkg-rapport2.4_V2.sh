85bde8316409829661e0733d5f462adf871bc9bc
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2017-02-13 14:35:56 +0100

    Update wpkg-rapport2.4_V2.sh

--- wpkg-rapport2.4_V2.sh
+++ wpkg-rapport2.4_V2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#installation de wpkg-report pour Eole 2.4 !!
-#version 09/2014
+# installation de wpkg-report pour Eole 2.4 !!
+# version 09/2014
 
 WPKGDIR="/home/wpkg"
 WPKGRAPPORT="$WPKGDIR/rapports"
@@ -9,7 +9,7 @@ URL='ftp://ftp.gig-mbh.de/software/wpkg-package-states.zip'
 FILE='wpkg-package-states.zip'
 nom_machine=`CreoleGet smb_netbios_name`
 
-#on teste la présence de eole-wpkg
+# on teste la présence de eole-wpkg
 dpkg -s eole-wpkg &>/dev/null
 if [ $? != 0 ] ; then
     echo "Wpkg n'est pas installé sur ce serveur !"
@@ -21,7 +21,6 @@ else
     setfacl -Rdm u::rwx,g::rwx,o::rwx .EtatsClients
     mkdir -p $WPKGRAPPORT &>/dev/null
     cd $WPKGRAPPORT &>/dev/null
-   # wget ftp://ftp.gig-mbh.de/software/wpkg-create-report.zip &>/dev/null
     wget $URL
     if [ $? != 0 ] ;then
         echo "l'archive n'a pas pu être téléchargée !"
@@ -30,8 +29,6 @@ else
         echo "Téléchargement de l'archive OK !"
         unzip -o $FILE
         rm -rf $FILE
-        #unzip -u wpkg-create-report.zip
-        #rm -rf wpkg-create-report.zip
         sed -i 's/<wpkgBaseDir><\/wpkgBaseDir>/<wpkgBaseDir>\\\\'$nom_machine'\\wpkg\\<\/wpkgBaseDir>/g' settings.xml
         sed -i 's/<clientStateStorePath>clientStates/<clientStateStorePath>\\\\'$nom_machine'\\wpkg\\softwares\\.EtatsClients\\/g' settings.xml
         sed -i 's/<ignoreExecuteAlways>false/<ignoreExecuteAlways>/g' settings.xml
