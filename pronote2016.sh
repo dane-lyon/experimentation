17703a01aab15178586e8da08a6b44350811ca9b
Simon.B <sibe39@users.noreply.github.com>
2017-05-29 16:09:00 +0200

    Delete pronote2016.sh

--- pronote2016.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# en considérant que le PoL partagé se trouve dans /home/.PlayOnLinux (a adapter suivant situation)
-
-moi=`whoami`
-sudo chown -R $moi:$moi /home/.PlayOnLinux
-/usr/share/playonlinux/playonlinux --run "Pronote 2016" %F
